34b961db05405a0860e39ef3f3c4f72097e5430f
kamalika Majumder <kamalikamajumder@kamalikas-MacBook-Air.local>
2017-03-13 16:38:17 +0530

    Kamalika | adding clean attributes file

--- Chef/cookbooks/infra/attributes/default.rb
+++ Chef/cookbooks/infra/attributes/default.rb
@@ -1,5 +1,6 @@
 default['infra']['databag_item'] = 'infra'
 default['infra']['databag_name'] = 'prod'
+default['infra']['bootstrap']['databag_item'] = 'bootstrap'
 default['infra']['env_prefix'] = 'prod'
 default['infra']['app_prefix'] = 'rb'
 default['infra']['network_acl_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_acl"
@@ -19,25 +20,22 @@ default['infra']['web_int_elb_name'] = "#{node['infra']['app_prefix']}-#{node['i
 
 default['infra']['device_name'] = '/dev/sda1'
 
-default['infra']['pub_instance_type'] = 't2.medium'
-default['infra']['web_app_instance_type'] = 't2.medium'
-default['infra']['web_api_instance_type'] = 't2.medium'
-default['infra']['svc_worker_instance_type'] = 't2.medium'
+default['infra']['instance_type'] = 't2.medium'
 
 #Server Names
-default['infra']['web_app_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_web_app_srv"
-default['infra']['web_api_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_web_public_app_srv"
-default['infra']['svc_worker_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_svc_worker_srv"
-default['infra']['pub_app_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_http_proxy_srv"
+default['infra']['pub_proxy_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_pub_http_proxy"
+default['infra']['web_app_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_web_appserver"
+default['infra']['web_public_app_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_web_public_appserver"
+
+default['infra']['svc_worker_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_svc_worker"
+
+default['infra']['svc_help_app_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_svc_help_app"
+default['infra']['svc_help_app_worker_srv_name'] = "#{node['infra']['app_prefix']}_#{node['infra']['env_prefix']}_svc_help_app_worker"
 
 default['infra']['image_id'] = 'ami-386e4350'
 
 default['infra']['proxy_srv_image_id'] = 'ami-43271154'
-default['infra']['key_name'] = 'ssh-default'
-default['infra']['ssh_user_name'] = 'ec2-user'
-
 default['infra']['instance_iam_role'] = 'rb_role_instance'
-default['infra']['pub_app_srv_runlist'] = "recipe['nginx_proxy']"
 
 #RDS
 
@@ -48,3 +46,31 @@ default['infra']['mysql_data_srv_name'] = "#{node['infra']['app_prefix']}-#{node
 default['infra']['psql_db_instance_type'] = "db.t2.medium"
 default['infra']['mysql_db_instance_type'] = "db.t2.medium"
 
+#Bootstrap
+
+default['infra']['facter_env_name'] = 'production'
+
+##appserver
+default['infra']['appserver_machine_hostname'] = 'appserver'
+default['infra']['appserver_machine_type'] = 'appserver'
+default['infra']['appserver_machine_name'] =  ''
+
+##public-appserver
+default['infra']['public_appserver_machine_hostname'] = 'public-appserver'
+default['infra']['public_appserver_machine_type'] = 'public-appserver'
+default['infra']['public_appserver_machine_name'] =  ''
+
+##worker
+default['infra']['worker_machine_hostname'] = 'worker'
+default['infra']['worker_machine_type'] = 'worker'
+default['infra']['worker_machine_name'] =  ''
+
+##help-app
+default['infra']['helpapp_machine_hostname'] = ''
+default['infra']['helpapp_machine_type'] = ''
+default['infra']['helpapp_machine_name'] =  ''
+
+##help-app-worker
+default['infra']['helpapp_worker_machine_hostname'] = ''
+default['infra']['helpapp_worker_machine_type'] = ''
+default['infra']['helpapp_worker_machine_name'] =  ''
